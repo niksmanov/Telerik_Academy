f702de13a472e489b130a1a8f833781753ab80ba
niksmanov <niks.manov@gmail.com>
2017-04-12 12:06:51 +0300

    Telerik Academy

---
+++ JavaScript-Applications/03.Handlebars/public/scripts/template-loader.js
@@ -0,0 +1,20 @@
+const templateLoader = (() => {
+    const templatesCache = {};
+
+    function get(templateName) {
+        return new Promise((resolve, reject) => {
+            if (templatesCache[templateName]) {
+                resolve(Handlebars.compile(templatesCache[templateName]));
+            }
+
+            $.get(`./templates/${templateName}.handlebars`, template => {
+                templatesCache[templateName] = template;
+                resolve(Handlebars.compile(template));
+            })
+        })
+    }
+
+    return { get };
+})();
+
+export { templateLoader };
